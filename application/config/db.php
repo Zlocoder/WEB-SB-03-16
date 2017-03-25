9c701427f0edbe0eb829bc0bde509703e93b83b7
Zlocoder <george.lemish@gmail.com>
2017-03-25 17:14:14 +0200

    universal db config

--- application/config/db.php
+++ application/config/db.php
@@ -1,9 +1,19 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii-shop',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yii-shop',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ];
+} elseif ($_SERVER['HTTP_HOST'] == 'george4e.beget.tech') {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yii-shop',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ];
+}
